a80276eed4399d99c04b9a7637d7453099fc7720
Moreno B <moreno_bralts@hotmail.com>
2016-12-06 10:39:34 +0100

    Added controller thread & stopping

--- WarDrivePiCar/Controller/Controller.py
+++ WarDrivePiCar/Controller/Controller.py
@@ -1,6 +1,20 @@
-class Controller:
+from threading import Thread
+from time import sleep
+
+
+class Controller(Thread):
+    __CPU_CYCLE_TIME = 0.05  # 50 ms
+    __running = False
+
     def __init__(self):
+        Thread.__init__(self)
         self.startupText = "Hello!"
 
-    def start(self):
-        print(self.startupText)
+    def run(self):
+        self.__running = True
+
+        while self.__running:
+            sleep(self.__CPU_CYCLE_TIME)
+
+    def _stop(self):
+        self.__running = False
