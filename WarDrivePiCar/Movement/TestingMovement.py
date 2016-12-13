bea6f558ce4e3677292a420b9f0a72c1ebeb280d
Moreno B <moreno_bralts@hotmail.com>
2016-12-13 13:46:01 +0100

    TestingMovement script will now make use of our updated Initio.py library.

--- WarDrivePiCar/Movement/TestingMovement.py
+++ WarDrivePiCar/Movement/TestingMovement.py
@@ -1,14 +1,12 @@
-from Movement import initio
+import initio
 from time import sleep
 
 try:
-    # Start the controller
-    initio.init()
     # Block the main thread
     while True:
         sleep(0.1)
-        initio.forward(50)
+        initio.forward(15)
 except KeyboardInterrupt:
     # On keyboard interrupt cleanup GPIO pins and shutdown initio library functions
-    initio.cleanup()
+    initio.stop()
 
