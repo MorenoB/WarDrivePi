9b225f6de986130c391d31e1045554c9cf95e67f
Moreno B <moreno_bralts@hotmail.com>
2016-12-14 16:05:35 +0100

    Moved setup.py to root git folder

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='WarDrivePiCar',
+    version='1.0',
+    packages=['', 'WarDrivePiCar\Tests', 'WarDrivePiCar\Sniffer', 'WarDrivePiCar\Movement', 'WarDrivePiCar\Controller',
+              'WarDrivePiCar\Communication'],
+    url='',
+    license='',
+    author='Jelmer & Moreno',
+    author_email='',
+    description=''
+)
