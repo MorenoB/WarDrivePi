8d57a744d5f55fb652376f0ecc21e520b0f2d9e7
Moreno B <moreno_bralts@hotmail.com>
2016-12-14 11:13:21 +0100

    Revert "TestingMovement script will now make use of our updated Initio.py library."
    This reverts commit bea6f558ce4e3677292a420b9f0a72c1ebeb280d.

--- WarDrivePiCar/Movement/TestingMovement.py
+++ WarDrivePiCar/Movement/TestingMovement.py
@@ -1,12 +1,14 @@
-import initio
+from Movement import initio
 from time import sleep
 
 try:
+    # Start the controller
+    initio.init()
     # Block the main thread
     while True:
         sleep(0.1)
-        initio.forward(15)
+        initio.forward(50)
 except KeyboardInterrupt:
     # On keyboard interrupt cleanup GPIO pins and shutdown initio library functions
-    initio.stop()
+    initio.cleanup()
 
