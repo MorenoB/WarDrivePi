2e2fbf9e7ee387928175cd3cb318046622fb6b27
Moreno B <moreno_bralts@hotmail.com>
2016-12-14 15:56:29 +0100

    Added setup.py

---
+++ WarDrivePiCar/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='WarDrivePiCar',
+    version='1.0',
+    packages=['', 'Tests', 'Sniffer', 'Movement', 'Controller', 'Communication'],
+    url='',
+    license='',
+    author='Jelmer&Moreno',
+    author_email='',
+    description=''
+)
