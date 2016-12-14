dde85289254e4b1092cfd98a51276597838f1c69
Moreno B <moreno_bralts@hotmail.com>
2016-12-14 16:44:36 +0100

    Added package_dir in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='WarDrivePiCar',
     version='1.0',
-    packages=['WarDrivePiCar', 'WarDrivePiCar.Tests', 'WarDrivePiCar.Sniffer', 'WarDrivePiCar.Movement',
-              'WarDrivePiCar.Controller', 'WarDrivePiCar.Communication'],
+    packages=['Movement'],
+    package_dir={'Movement': 'WarDrivePiCar/Movement'},
     url='',
     license='',
     author='Jelmer & Moreno',
