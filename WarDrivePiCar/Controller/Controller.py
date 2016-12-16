1cbfe33271a91351ae3d9961a90cac3a4c80e9c1
Moreno B <moreno_bralts@hotmail.com>
2016-12-16 12:20:27 +0100

    Implemented sensor trigger events from the initio library

--- WarDrivePiCar/Controller/Controller.py
+++ WarDrivePiCar/Controller/Controller.py
@@ -13,12 +13,25 @@ class Controller(Thread):
     def run(self):
 
         self.__carMovement = Initio()
+        self.__carMovement.forward(50)
+
+        # Register events
+        self.__carMovement.onRightEncoderTriggered += self.__print_number_of_pulses
+        self.__carMovement.onLeftEncoderTriggered += self.__print_number_of_pulses
+
+        # Main Loop
         while not self.name.endswith("--"):
             sleep(self.__CPU_CYCLE_TIME)
 
-            self.__carMovement.forward(50)
-
         self.__shutdown_controller()
+
+        #  De register events
+        self.__carMovement.onRightEncoderTriggered -= self.__print_number_of_pulses
+        self.__carMovement.onLeftEncoderTriggered -= self.__print_number_of_pulses
+
+    def __print_number_of_pulses(self):
+        print "Left pulses: ", self.__carMovement.numberOfLeftPulses,\
+            " Right Pulses: ", self.__carMovement.numberOfRightPulses
 
     def __shutdown_controller(self):
         print "Shutting down controller..."
