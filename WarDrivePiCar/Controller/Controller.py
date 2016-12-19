2c7bca3e3274da127f16b8cf2ace51176dbf194a
Moreno B <moreno_bralts@hotmail.com>
2016-12-19 11:31:32 +0100

    Added car control using keyboard events.

--- WarDrivePiCar/Controller/Controller.py
+++ WarDrivePiCar/Controller/Controller.py
@@ -2,22 +2,52 @@ from threading import Thread
 from time import sleep
 from Movement.initio import Initio
 from pubsub import pub
+from pynput.keyboard import Key, Listener
+
 
 class Controller(Thread):
     __CPU_CYCLE_TIME = 0.05  # 50 ms
     __carMovement = None
+    __carSpeed = 15
 
     def __init__(self):
         Thread.__init__(self)
 
-    def run(self):
-
+        # Initialise car hardware library.
         self.__carMovement = Initio()
-        self.__carMovement.forward(50)
 
         # Register events
         pub.subscribe(self.__print_number_of_left_pulses, self.__carMovement.EVENT_ON_LEFT_ENCODER)
         pub.subscribe(self.__print_number_of_right_pulses, self.__carMovement.EVENT_ON_RIGHT_ENCODER)
+
+        # Collect events until released
+        with Listener(
+                on_press=self.__on_press,
+                on_release=self.__on_release) as listener:
+            listener.join()
+
+    def __on_press(self, key):
+
+        # Move the car according to directional keyboard input events
+        if key == Key.up:
+            self.__carMovement.forward(self.__carSpeed)
+
+        if key == Key.down:
+            self.__carMovement.reverse(self.__carSpeed)
+
+        if key == Key.left:
+            self.__carMovement.spin_left(self.__carSpeed)
+
+        if key == Key.right:
+            self.__carMovement.spin_right(self.__carSpeed)
+
+    def __on_release(self, key):
+
+        # Car needs to stop moving when a key is being released.
+        if key == Key.up or key == Key.down or key == Key.left or key == Key.right:
+            self.__carMovement.stop()
+
+    def run(self):
 
         # Main Loop
         while not self.name.endswith("--"):
@@ -34,3 +64,6 @@ class Controller(Thread):
     def __shutdown_controller(self):
         print "Shutting down controller..."
         self.__carMovement.cleanup()
+
+        # Stop keyboard listener.
+        Listener.stop()
