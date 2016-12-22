8a7e109dead08ac7f2bdc3404063c82ae2c3e253
Moreno B <moreno_bralts@hotmail.com>
2016-12-22 14:09:06 +0100

    Updated setup.py to support latest 'Utils' folder addition. Also updated requirements file to update the py-test dependency

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='WarDrivePiCar',
     version='1.0',
-    packages=['Movement'],
-    package_dir={'Movement': 'WarDrivePiCar/Movement'},
+    packages=['Movement', 'Util'],
+    package_dir={'Movement': 'WarDrivePiCar/Movement', 'Util': 'WarDrivePiCar/Util'},
     url='',
     license='',
     author='Jelmer & Moreno',
