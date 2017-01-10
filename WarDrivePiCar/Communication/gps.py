5091cdf375dd77fb697e1c41aa3c14685a726bc1
Moreno B <moreno_bralts@hotmail.com>
2017-01-10 19:00:12 +0100

    Refactored methods in gps.py and added more documentation

--- WarDrivePiCar/Communication/gps.py
+++ WarDrivePiCar/Communication/gps.py
@@ -12,6 +12,10 @@ class GPS(Thread):
     __average_longitude = 0
     __average_latitude = 0
 
+    # dumpsys terminology
+    __term_longitude = "mLongitude="
+    __term_latitude = "mLatitude="
+
     # Event names
     EVENT_ON_LATITUDE_CHANGED = "OnLatitudeChanged"
     EVENT_ON_LONGITUDE_CHANGED = "OnLongitudeChanged"
@@ -20,9 +24,10 @@ class GPS(Thread):
     testing_input = ""
 
     def __init__(self):
-        # adb usb
         try:
+            # Boot up adb through USB using the command 'adb usb'
             call(["adb", "usb"])
+
         # Still allow the thread to properly start and disregard any exceptions for now.
         except OSError:
             pass
@@ -34,7 +39,6 @@ class GPS(Thread):
         while not self.name.endswith("--"):
             sleep(self.__GPS_POLL_TIME)
 
-            # adb dumpsys location > Dumps/location.txt
             try:
 
                 # Used in Unit-Tests
@@ -42,8 +46,10 @@ class GPS(Thread):
                     self.__retrieve_location_information(self.testing_input)
                     self.testing_input = ""
 
+                # Execute command 'adb dumpsys location' and redirect output to our methods.
                 raw_location_output = check_output(["adb", "shell", "dumpsys", "location"])
                 self.__retrieve_location_information(raw_location_output)
+
             except OSError:
                 print "'adb' Command not properly installed on this machine! Shutting down GPS module..."
                 break
@@ -55,35 +61,41 @@ class GPS(Thread):
 
     def __retrieve_location_information(self, raw_location_output):
         location_providers = []
+
+        # First retrieve all location providers.
+        # Location provider examples : GPS, Network, Cellular etc...
         for item in raw_location_output.split("\n"):
 
             # Sometimes the android log will also display a coordinate which is a duplicate.
             if "LOG" in item:
                 continue
 
-            if "mLatitude=" in item:
+            if self.__term_latitude in item:
                 location_providers.append(item.strip())
 
         if len(location_providers) < 1:
             return
 
+        # Seems like we have got fresh new data, delete the old first.
         self.__latitudes = []
         self.__longitudes = []
 
+        # Go through every location provider and only get the longitude and latitude values and store them.
         for location_provider in location_providers:
             suffix = location_provider.split(" ")
             for item in suffix:
-                if "mLatitude=" in item:
-                    latitude_string_value = item.replace("mLatitude=", "")
+                if self.__term_latitude in item:
+                    latitude_string_value = item.replace(self.__term_latitude, "")
                     self.__latitudes.append(latitude_string_value)
 
-                if "mLongitude=" in item:
-                    longitude_string_value = item.replace("mLongitude=", "")
+                if self.__term_longitude in item:
+                    longitude_string_value = item.replace(self.__term_longitude, "")
                     self.__longitudes.append(longitude_string_value)
 
-        self.__print_average_coordinates()
+        # Now we just have to update our average coordinates
+        self.__calculate_and_update_average_coordinates()
 
-    def __print_average_coordinates(self):
+    def __calculate_and_update_average_coordinates(self):
         average_longitude = 0
         average_latitude = 0
 
@@ -95,6 +107,8 @@ class GPS(Thread):
 
         average_longitude /= len(self.__longitudes)
         average_latitude /= len(self.__latitudes)
+
+        # When a change in average latitude/longitude is detected, notify all registered event handlers.
 
         if average_latitude != self.__average_latitude:
             self.__average_latitude = average_latitude
