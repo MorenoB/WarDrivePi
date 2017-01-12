0e15e26d6f70280359161e7e8f78566d9db471da
Jelmer <jelmer_hulsman@hotmail.com>
2017-01-12 13:01:37 +0100

    Packages who previously printed all it's payload are now committed to the database

--- WarDrivePiCar/__main__.py
+++ WarDrivePiCar/__main__.py
@@ -1,22 +1,23 @@
 import os
 import sys
 import pydevd
-import SysArgv
-import Main
+
+from SysArgv import SysArgv
+from Main import Main
 
 if __name__ == "__main__":
     print "\nPython {0}\n".format(sys.version)
 
-    SysArgv = SysArgv.SysArgv()
+    sys_argv = SysArgv()
 
-    if not SysArgv.validate():
-        SysArgv.print_argv()
+    if not sys_argv.validate():
+        sys_argv.print_argv()
         print "Invalid arguments! Exited."
         exit()
 
-    if "trace" in SysArgv.items:
+    if "trace" in sys_argv.items:
         pydevd.settrace(
-            host=SysArgv.items['trace'],
+            host=sys_argv.items['trace'],
             stdoutToServer=True,
             stderrToServer=True,
             suspend=False,
@@ -24,12 +25,12 @@ if __name__ == "__main__":
         )
 
         if not pydevd.connected:
-            print "Couldn't not reach REMOTE DEBUGGER on '{0}'!".format(SysArgv.items['trace'])
-            os.system("ping -c 1 {0}".format(SysArgv.items['trace']))
+            print "Couldn't not reach REMOTE DEBUGGER on '{0}'!".format(sys_argv.items['trace'])
+            os.system("ping -c 1 {0}".format(sys_argv.items['trace']))
 
-    # Boot up program
-    program = Main.Main()
-    program.start()
+    # Boot
+    main = Main()
+    main.start()
 
     if pydevd.connected:
         pydevd.stoptrace()
