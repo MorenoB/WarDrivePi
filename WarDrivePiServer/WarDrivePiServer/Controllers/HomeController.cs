449af140497e8118cdc27c2a8e711da749fd6847
Jelmer <jelmer_hulsman@hotmail.com>
2017-01-18 11:56:40 +0100

    Added Marker model and fetched it's raw data from the database User is now displayed in the Leaflet map Added Leaflet.heat for later implementation

--- WarDrivePiServer/WarDrivePiServer/Controllers/HomeController.cs
+++ WarDrivePiServer/WarDrivePiServer/Controllers/HomeController.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using Npgsql;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NpgsqlTypes;
+using WarDrivePiServer.Models;
 
 namespace WarDrivePiServer.Controllers
 {
     public class HomeController : Controller
     {
+        private const string NpgsqlConnectionString = "Server=localhost;" +
+                                                      "Port=5432;" +
+                                                      "Username=postgres;" +
+                                                      "Password=g4Kzri^akWWP^LEQ0&Z%QRfW;" +
+                                                      "Database=packets;";
+
         public ActionResult Dashboard()
         {
             return View();
@@ -15,7 +27,51 @@ namespace WarDrivePiServer.Controllers
 
         public ActionResult Map()
         {
-            return View();
+            var baseTable = new DataTable();
+
+            using (var npgsqlConnection = new NpgsqlConnection(NpgsqlConnectionString))
+            using (var npgsqlCommand = new NpgsqlCommand(Marker.BaseQuery, npgsqlConnection))
+            {
+                npgsqlConnection.Open();
+
+                using (var npgsqlDataReader = npgsqlCommand.ExecuteReader())
+                {
+                    baseTable.Load(npgsqlDataReader);
+                }
+
+                npgsqlConnection.Close();
+            }
+
+            var markers = new ArrayList();
+            foreach (DataRow baseTableRow in baseTable.Rows)
+            {
+                var elementsTable = new DataTable();
+
+                using (var npgsqlConnection = new NpgsqlConnection(NpgsqlConnectionString))
+                using (var npgsqlCommand = new NpgsqlCommand(Marker.ElementsQuery, npgsqlConnection))
+                {
+                    npgsqlConnection.Open();
+
+                    npgsqlCommand.Parameters.AddWithValue("pointer", NpgsqlDbType.Bigint, (long)baseTableRow.ItemArray[0]);
+                    npgsqlCommand.Prepare();
+
+                    using (var npgsqlDataReader = npgsqlCommand.ExecuteReader())
+                    {
+                        elementsTable.Load(npgsqlDataReader);
+                    }
+
+                    npgsqlConnection.Close();
+                }
+
+                markers.Add(new Marker(baseTableRow, elementsTable));
+            }
+
+            foreach (var marker in markers)
+            {
+                
+            }
+
+            return View(markers);
         }
 
         public ActionResult Drive()
